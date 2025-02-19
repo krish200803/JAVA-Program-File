a1cd01c904a5f313d07961d78f29b6205ae0a0a4
krish200803 <umeshbhadane2020@gmail.com>
2025-02-19 18:37:52 +0530

    pattern
    Pattern programs

---
+++ Pattern Programs/Pattern2.java
@@ -0,0 +1,15 @@
+class Pattern2 
+{
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		for(int i=0; i<n-2; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+}
