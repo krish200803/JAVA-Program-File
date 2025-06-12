44945b46ae42ce82bbc11338bbbc2635bfb71dbb
krish200803 <umeshbhadane2020@gmail.com>
2025-06-12 22:15:40 +0530

    Doctor

---
+++ OOP Concept/Derived TypeCasting/Down Casting/Orthopedic.java
@@ -0,0 +1,25 @@
+package Inheritance.DerivedTypecasting.DownCasting;
+
+public class Orthopedic extends Doctor {
+	
+	String spl;
+	String organ;
+	
+	public Orthopedic(String name,double fees, String degree, int exp, String hospital, String spl, String organ)
+	{
+		super(name, fees, degree, exp, hospital);
+		
+		this.spl = spl;
+		this.organ = organ;
+	}
+	
+	public void displayOrthopedic() 
+	{
+		displayDoctor();
+		
+		System.out.println("Specialist :"+ spl);
+		System.out.println("Organ :"+ organ);
+	}
+
+
+}
