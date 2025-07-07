2180db358a969c0a4306bc98369ec56642c7c84a
krish200803 <umeshbhadane2020@gmail.com>
2025-07-07 11:19:28 +0530

    Wrapper Class Constants

---
+++ Wrapper Class/Constants.java
@@ -0,0 +1,32 @@
+package WrapperClass.Unboxing;
+
+public class Constants {
+
+	public static void main(String[] args) {
+		
+//		Wrapper Classes has a Static Final Variable i.e Constants which prints Csonstants of every datatype
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_EXPONENT);
+		System.out.println(Double.MIN_EXPONENT);
+		System.out.println(Double.MIN_NORMAL);
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_EXPONENT);
+		System.out.println(Float.MIN_EXPONENT);
+		
+		System.out.println(Character.MAX_VALUE);
+		System.out.println(Character.MIN_VALUE);
+	}
+
+}
+
+
