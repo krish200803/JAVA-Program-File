8520b0a0b1ed01aa746b6ce24b09e78e473a636d
krish200803 <umeshbhadane2020@gmail.com>
2025-08-01 15:13:42 +0530

    new keyword and String Constructor
    String Object using new keyword and String Constructor

---
+++ STRINGS/String_Progam2.java
@@ -0,0 +1,32 @@
+package STRINGS;
+
+public class String_Progam2 {
+
+	public static void main(String[] args) {
+		
+		
+		String s1 = "Hii";
+		
+		String s2 = "Hello";
+		
+		String s3 = "Hii";
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		System.out.println(s1==s3);    // True
+		
+		System.out.println(s1.equals(s3));   // True
+		
+		System.out.println(s1.hashCode());   // 72552
+		System.out.println(s2.hashCode());   // 69609650
+		System.out.println(s3.hashCode());   // 72552
+		
+		System.out.println(s1.hashCode()==s3.hashCode());    // True
+		
+		
+
+	}
+
+}
